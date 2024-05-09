643de9264fd2380050584a8ca50acccd6e98fbb9
Pascal95120 <140958680+Pascal95120@users.noreply.github.com>
2024-05-09 14:25:14 +0200

    Update CreateTable.java

--- CreateTable.java
+++ CreateTable.java
@@ -1,12 +1,20 @@
 import java.sql.*;
-import java.sql.DriverManager;
+public class CreateTable {
+    public static void main(String[] args) throws SQLException {
+        try {
+            //Modifier par l'emplacement de la base de donnée, là c'est la mienne
+            //Il faut créer une bdd à partir de database
+            Connection co = DriverManager.getConnection("jdbc:sqlite:C:/Users/CYTech Student/IdeaProjects/SQL/src/Personne");
+            Statement stmt = co.createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM Personne");
+            while(res.next()){
+                String name = res.getString("PRENOM");
+                int age = res.getInt("AGE");
+                System.out.println(name+" a "+age+" ans.");
 
-
-public class CreateTable
-{
-    public static void main(String[] args)
-    {
-        Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
-
+            }
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
