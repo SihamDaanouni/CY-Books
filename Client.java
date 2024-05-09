8baeb43099b7d9fb5ef256f8d084647bac330b81
Dracothomas <hautiontho@cy-tech.fr>
2024-05-09 12:08:58 +0200

    cllass
    xreation des class

---
+++ Client.java
@@ -0,0 +1,34 @@
+public class Client extends User {
+
+    public Client (String name,String FirstName,String address,String mail,String phone){
+
+        this.Name=name;
+        this.FirstName=FirstName;
+        this.address=address;
+        this.mail=mail;
+        this.phone=phone;
+        this.connected=false;
+        }/*
+
+        //getters, setters,
+/*
+        public void borrowBook(){
+
+        }
+
+        public void subscription(){
+
+        }
+
+        public void remainingBorrowPeriod(){
+
+        }*/
+
+
+}
+
+
+
+
+
+
