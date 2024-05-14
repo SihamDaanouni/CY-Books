e4b87ba6baab56538dd254082e51a9087dd7bcd0
Pascal95120 <140958680+Pascal95120@users.noreply.github.com>
2024-05-14 22:37:42 +0200

    Update book.java

--- book.java
+++ book.java
@@ -1,23 +1,20 @@
 import java.io.File;
-//import java.io.IOException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.net.http.HttpClient; //Pour envoyer des requêtes HTTP
-import java.net.http.HttpRequest; //Pour construire des requêtes HTTP
-import java.net.http.HttpResponse; //Pour recevoir des requêtes HTTP
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.sql.Connection;
-
 import java.sql.DriverManager;
-//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javax.xml.parsers.DocumentBuilder; //Créer un objet pour analyser le XML
-import javax.xml.parsers.DocumentBuilderFactory; //Créer un objet qui construit un DocumentBuilder pour le XML
+import java.util.Scanner;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -28,20 +25,21 @@ public class book {
 
     private String titre;
     private String auteur;
+    private String theme;
     private String isbn;
     private String edition;
     private String lieuPpublication;
-    private String  anneePublication;
+    private String anneePublication;
 
-    public book ( String titre,String auteur,String isbn,String edition , String lieuPpublication , String  anneePublication){
-        this.titre=titre;
-        this.auteur=auteur;
-        this.isbn=isbn;
-        this.edition=edition;
-        this.lieuPpublication=lieuPpublication;
-        this.anneePublication=anneePublication;
+    public book(String titre, String auteur, String theme, String isbn, String edition, String lieuPpublication, String anneePublication) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.theme = theme;
+        this.isbn = isbn;
+        this.edition = edition;
+        this.lieuPpublication = lieuPpublication;
+        this.anneePublication = anneePublication;
     }
-
 
     public String getTitre() {
         return titre;
@@ -57,6 +55,14 @@ public class book {
 
     public void setAuteur(String auteur) {
         this.auteur = auteur;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
     }
 
     public String getIsbn() {
@@ -90,84 +96,115 @@ public class book {
     public void setAnneePublication(String anneePublication) {
         this.anneePublication = anneePublication;
     }
-    public void recup(){
-        setTitre("relativité");
-        setAuteur("eistein");
 
+    public void recup() {
+        setTitre("relativité");
+        setAuteur("einstein");
     }
+
     @Override
-    public String toString(){
-        System.out.println(getTitre()+""+getAuteur()+""+getIsbn()+""+getEdition()+""+getLieuPpublication()+""+getAnneePublication());
+    public String toString() {
+        System.out.println(getTitre() + " " + getAuteur() + " " + getTheme() + " " + getIsbn() + " " + getEdition() + " " + getLieuPpublication() + " " + getAnneePublication());
         return null;
     }
 
-
-    public void recherche (String search) throws ParserConfigurationException, IOException, InterruptedException, SAXException, SQLException {
-        search= "bib.author all /"+search+"/";
-
-        // search="\"bib.author all \\\""+search+"\\\"\"";
+    public void recherche(String search) throws ParserConfigurationException, IOException, InterruptedException, SAXException, SQLException {
 
         File myFile = new File("Books");
         String chemin = "jdbc:sqlite:" + myFile.getAbsolutePath();
-        chemin = chemin.substring(0, chemin.length() - 5);
-        chemin = chemin + "src/Books";
-        // String titre;
-        // String auteur;
-        // String varchar;
-        int j=0;
+        chemin = chemin.substring(0, chemin.length() - 5) + "src/Books";
+        int j = 0;
+        String query = "";
+        String query1 = "";
+        String query2 = "";
+        int valeur2 = 0;
 
-        //Ce qu'on cherche + s'assure que les caractères spéciaux sont bien formatés pour une URL
-        //Lire le PDF2 au sujet des critères de recherches
-        //Il faudra modifier ces deux lignes suivantes en fonction de ce qu'on veut chercher (titre, auteur, annee...)
+        while (valeur2 != 3) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Entrez un critère de recherche (0: anywhere, 1: author, 2: title, 3: subject, 4: isbn, 5: publisher, 6: publication place, 7: publication date): ");
+            int valeur = sc.nextInt();
 
-        //METTRE EN COMMENTAIRE LES AUTRES LIGNES CAR ON A UN SEUL QUERY, DANS CE CODE JE CHERCHE PAR LE TITRE
-        //AUTEUR
-        String query = URLEncoder.encode("bib.author all \"Arkady Martine\"", StandardCharsets.UTF_8);
-        //TITRE
-        //String query = URLEncoder.encode("bib.title all \"MPSI\"", StandardCharsets.UTF_8.toString());
+            while (valeur < 0 || valeur > 7) {
+                sc = new Scanner(System.in);
+                System.out.println("Entrez un critère de recherche (0: anywhere, 1: author, 2: title, 3: subject, 4: isbn, 5: publisher, 6: publication place, 7: publication date): ");
+                valeur = sc.nextInt();
+            }
 
-        //C'est l'URL de base, toutes les requêtes commence par ça, pour faire les requêtes dans l'API de la BNF
+            Scanner sc1 = new Scanner(System.in);
+            System.out.println("Tape ton mot");
+            search = sc1.nextLine();
+            query1 = "(";
+
+            switch (valeur) {
+                case 0:
+                    query1 += "bib.anywhere all \"" + search + "\"";
+                    break;
+                case 1:
+                    query1 += "bib.author all \"" + search + "\"";
+                    break;
+                case 2:
+                    query1 += "bib.title all \"" + search + "\"";
+                    break;
+                case 3:
+                    query1 += "bib.subject all \"" + search + "\"";
+                    break;
+                case 4:
+                    query1 += "bib.isbn all \"" + search + "\"";
+                    break;
+                case 5:
+                    query1 += "bib.publisher all \"" + search + "\"";
+                    break;
+                case 6:
+                    query1 += "bib.publicationplace all \"" + search + "\"";
+                    break;
+                case 7:
+                    query1 += "bib.date all \"" + search + "\"";
+                    break;
+            }
+
+            query += query1;
+            System.out.println("Entrez une option, (1 pour AND et 3 pour terminer la recherche)");
+
+            Scanner sc3 = new Scanner(System.in);
+            valeur2 = sc3.nextInt();
+
+            while (valeur2 != 1 && valeur2 != 3) {
+                sc3 = new Scanner(System.in);
+                valeur2 = sc3.nextInt();
+            }
+
+            query2 = (valeur2 == 1) ? ") and " : ")";
+            query += query2;
+            System.out.println(query);
+        }
+
+        String queryfinal = URLEncoder.encode(query, StandardCharsets.UTF_8);
         String baseUri = "http://catalogue.bnf.fr/api/SRU";
-        //On commence à compter le nombre de documents
         int startRecord = 1;
-        //On affichera les documents 10 par 10
         int recordsPerRequest = 300;
-
-        //Pour envoyer les requêtes HTTP
         HttpClient client = HttpClient.newHttpClient();
 
-        //On continue tant qu'il reste des documents à trouver
         while (true) {
-            //Création de l'URL pour faire les requêtes (voir le pdf de la BNF)
-            //
             String requestUri = String.format("%s?version=1.2&operation=searchRetrieve&query=%s&startRecord=%d&maximumRecords=%d",
-                    baseUri, query, startRecord, recordsPerRequest);
+                    baseUri, queryfinal, startRecord, recordsPerRequest);
 
-            //Création d'une requête HTTP GET avec l'URL de la requête
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(requestUri))
                     .GET()
                     .build();
 
-            //Envoie de la requête et récupération de la réponse
-            //Attention les réponse qu'on reçoit sont écrites en XML il faut les traduire dans notre langage grâce à des fonctions des bibliothèques importés
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
             if (response.statusCode() == 200) {
-                //Création d'une instance pour stocker les documents trouvés
-                //On lit la réponse XML et on transforme cette réponse en un format plus facile comprendre et à chercher(tags...)
                 DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
                 DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-                //On met les valeurs obtenu dans le document au-dessus
                 Document doc = dBuilder.parse(new java.io.ByteArrayInputStream(response.body().getBytes(StandardCharsets.UTF_8)));
-                //Normalise le document
                 doc.getDocumentElement().normalize();
-                //Récupère tous les noeuds enfants du docuement ayant la balise "srw:record" chaque livre en a un
                 NodeList records = doc.getElementsByTagName("srw:record");
                 int recordsCount = records.getLength();
 
                 if (recordsCount == 0) {
-                    break; // No more records, exit the loop
+                    break;
                 }
 
                 startRecord += recordsCount;
@@ -177,23 +214,20 @@ public class book {
                     if (node.getNodeType() == Node.ELEMENT_NODE) {
                         Element element = (Element) node;
                         String isbn = getElementByTag(element, "010", "a");
-                        //Ne prend que les documents ayant un ISBN sinon il le passe.
-                        if (!isbn.equals("Non trouvé")) {
-                            j+=1;
 
-                            String title = getElementByTag(element, "200", "a");
-                            title=title.replace("'"," ");
-                            String author = getElementByTag(element, "700", "a");
-                            author=author.replace("'"," ");
-                            String publisher = getElementByTag(element, "210", "c");
-                            publisher=publisher.replace("'"," ");
-                            String publicationPlace = getElementByTag(element, "210", "a");
-                            publicationPlace=publicationPlace.replace("'"," ");
-                            String publicationYear = getElementByTag(element, "210", "d");
-                            publicationYear=publicationYear.replace("'"," ");
+                        if (!isbn.equals("Non trouvé")) {
+                            j++;
+
+                            String title = getTitle(element).replace("'", " ");
+                            String author = getAuthor(element).replace("'", " ");
+                            String theme = getTheme(element).replace("'", " ");
+                            String publisher = getPublisher(element).replace("'", " ");
+                            String publicationPlace = getPublicationPlace(element).replace("'", " ");
+                            String publicationYear = getPublicationYear(element).replace("'", " ");
 
                             System.out.print("Titre: " + title);
                             System.out.print(" Auteur: " + author);
+                            System.out.print(" Thème: " + theme);
                             System.out.print(" ISBN: " + isbn);
                             System.out.print(" Éditeur: " + publisher);
                             System.out.print(" Lieu de publication: " + publicationPlace);
@@ -201,87 +235,126 @@ public class book {
 
                             Connection co = DriverManager.getConnection(chemin);
                             Statement stmt = co.createStatement();
-                            String SQL = ("INSERT INTO Books VALUES ('"+j+"','"+title+"','"+author+"','"+isbn+"','"+publisher+"','"+publicationPlace+"','"+publicationYear+"')");
+                            String SQL = ("INSERT INTO Books VALUES ('" + j + "','" + title + "','" + author + "','" + theme + "','" + isbn + "','" + publisher + "','" + publicationPlace + "','" + publicationYear + "')");
                             stmt.executeUpdate(SQL);
                             co.close();
                         }
                     }
-
                 }
             } else {
                 System.out.println("Failed to fetch data: HTTP Error " + response.statusCode());
                 Connection co = DriverManager.getConnection(chemin);
                 Statement stmt = co.createStatement();
-                // String SQL = ("DROP table Books ");
-                // stmt.executeUpdate(SQL);
-                  /*  SQL = ("CREATE TABLE Books (    compteur int,\n" +
-                            "                        titre VARCHAR(255),\n" +
-                            "                        auteur VARCHAR(255),\n" +
-                            "                        isbn VARCHAR(255),\n" +
-                            "                        edition VARCHAR(255),\n" +
-                            "                        lieu_publication VARCHAR(255),\n" +
-                            "                        annee_publication VARCHAR(255)\n" +
-                            "); ");
-                    stmt.executeUpdate(SQL);
-                    SQL = ("INSERT INTO Books VALUES ('"+j+"','Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'No')");
-
-
-                    stmt.executeUpdate(SQL);
-                    */
-
                 co.close();
-
-                break; // Exit the loop on HTTP errors other than 200
+                break;
             }
         }
-
-
     }
 
-
-    //Méthode pour extraire les données XML en un truc compréhensible
-    //Un élément XML contient un tag puis un code dans le tag
-
     private static String getElementByTag(Element element, String tag, String code) {
-        //Fonction qui cherche les tag commençant par "mxc:datafield"
-        //J'ai fait ça pour que la sortie soit compréhensible
-        //La sortie brut en XML est difficile à comprendre
-        //LA METHODE getElementByTag est très importante pour la lisibilité
-        //J'ai fait un fichier nommée TestBNFAPI.java pour voir ce que ça donne
         NodeList fields = element.getElementsByTagName("mxc:datafield");
-        //On parcourt tous les éléments dans la liste
         for (int i = 0; i < fields.getLength(); i++) {
-            //On sélectionne le premier arrivé
             Element field = (Element) fields.item(i);
-            //On cherche la ligne avec le mot tag pour continuer, ensuite on cherche à l'intérieur de tag
             if (field.getAttribute("tag").equals(tag)) {
-                //Fonction qui cherche un sous élément de tag qui se nomme mxc:subfield
-                //Comment je sais que c'est ça, comme dit ci-dessus met la méthode getElementByTag en commentaire et exécute
-                //Tu verras une sortie difficile à comprendre, il faut extraire les bonnes parties dans la sortie
                 NodeList subFields = field.getElementsByTagName("mxc:subfield");
-                //On refait une boucle dans le subfield, pour trouver le code
                 for (int j = 0; j < subFields.getLength(); j++) {
-                    //On prend le premier élément
                     Element subField = (Element) subFields.item(j);
-                    //On vérifie qu'il y a bien le code
                     if (subField.getAttribute("code").equals(code)) {
                         return subField.getTextContent();
                     }
                 }
             }
         }
-        //Pour résumer il faut le tag et le code qui se trouve dans une liste qui contient des sous listes
-        //Dans notre cas, on cherche que jusqu'à la 2ème sous liste
-        //200a c'est pour le titre
-        //700a c'est pour le nom de l'auteur
-        //210c c'est le nom de l'éditeur
-        //210a c'est le lieu de la publication
-        //210d c'est l'année de publication
-        //SI VOUS VOULEZ EN SAVOIR PLUS CHERCHER "catalogage MARC", langage utilisé pour les bibliothèques pour trier et classer les livres.
         return "Non trouvé";
     }
+
+    private static String getTitle(Element element) {
+        // Essayez plusieurs tags pour trouver le titre
+        String[] tags = {"200", "245"};
+        String title = "Non trouvé";
+
+        for (String tag : tags) {
+            title = getElementByTag(element, tag, "a");
+            if (!title.equals("Non trouvé")) {
+                break;
+            }
+        }
+
+        return title;
+    }
+
+    private static String getAuthor(Element element) {
+        // Essayez plusieurs tags pour trouver l'auteur
+        String[] tags = {"700", "100"};
+        String author = "Non trouvé";
+
+        for (String tag : tags) {
+            author = getElementByTag(element, tag, "a");
+            if (!author.equals("Non trouvé")) {
+                break;
+            }
+        }
+
+        return author;
+    }
+
+    private static String getTheme(Element element) {
+        // Essayez plusieurs tags pour trouver le thème
+        String[] tags = {"600", "606", "607", "610"};
+        String theme = "Non trouvé";
+
+        for (String tag : tags) {
+            theme = getElementByTag(element, tag, "a");
+            if (!theme.equals("Non trouvé")) {
+                break;
+            }
+        }
+
+        return theme;
+    }
+
+    private static String getPublisher(Element element) {
+        // Essayez plusieurs tags pour trouver l'éditeur
+        String[] tags = {"210", "260"};
+        String publisher = "Non trouvé";
+
+        for (String tag : tags) {
+            publisher = getElementByTag(element, tag, "c");
+            if (!publisher.equals("Non trouvé")) {
+                break;
+            }
+        }
+
+        return publisher;
+    }
+
+    private static String getPublicationPlace(Element element) {
+        // Essayez plusieurs tags pour trouver le lieu de publication
+        String[] tags = {"210", "260"};
+        String publicationPlace = "Non trouvé";
+
+        for (String tag : tags) {
+            publicationPlace = getElementByTag(element, tag, "a");
+            if (!publicationPlace.equals("Non trouvé")) {
+                break;
+            }
+        }
+
+        return publicationPlace;
+    }
+
+    private static String getPublicationYear(Element element) {
+        // Essayez plusieurs tags pour trouver l'année de publication
+        String[] tags = {"210", "260"};
+        String publicationYear = "Non trouvé";
+
+        for (String tag : tags) {
+            publicationYear = getElementByTag(element, tag, "d");
+            if (!publicationYear.equals("Non trouvé")) {
+                break;
+            }
+        }
+
+        return publicationYear;
+    }
 }
-
-
-
-
