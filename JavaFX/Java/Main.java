b7439147a43783de1df84c8e919b0127d38e87d4
Pascal95120 <140958680+Pascal95120@users.noreply.github.com>
2024-05-22 00:28:25 +0200

    Update Main.java

--- JavaFX/Java/Main.java
+++ JavaFX/Java/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Scene1.fxml"));
         //Email: Eva@gmal.com
-        //MDP: 2548 ou 2458 c'est l'un des deux
+        //MDP: 2548 
         Scene scene = new Scene(fxmlLoader.load());
         Image icon = new Image(getClass().getResourceAsStream("/com/example/cybooks/logocy.png"));
         stage.getIcons().add(icon);
