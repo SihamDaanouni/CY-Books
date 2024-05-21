c8004f2556e4e9e3938a3b9d8ec2194ef0c42271
Pascal95120 <140958680+Pascal95120@users.noreply.github.com>
2024-05-22 00:11:44 +0200

    Update Scene3Controller.java

--- JavaFX/Java/Scene3Controller.java
+++ JavaFX/Java/Scene3Controller.java
@@ -1,4 +1,36 @@
 package com.example.cybook;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Scene3Controller {
+    private Stage stage;
+    private Scene scene;
+    public void switchToScene6(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene6.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene7(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene7.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene8(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene8.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
