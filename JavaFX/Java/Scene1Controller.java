fadf02a5526f6cc1d1c18ee45ab5a52a076b53ff
Pascal95120 <140958680+Pascal95120@users.noreply.github.com>
2024-05-22 00:11:14 +0200

    Update Scene1Controller.java

--- JavaFX/Java/Scene1Controller.java
+++ JavaFX/Java/Scene1Controller.java
@@ -15,16 +15,8 @@ import java.io.IOException;
 public class Scene1Controller {
     private Stage stage;
     private Scene scene;
-    private Parent root;
-
-    @FXML
-    private TextField mailField; // Pour la scène connexion
-    @FXML
-    private PasswordField passwordField; // Pour la scène connexion
-
-    // Scène 1
     public void switchToConnexion(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybook/Scene2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene2.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
