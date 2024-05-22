b2245de6c7fe51dbf4f461b63ccead493af7701b
Pascal95120 <140958680+Pascal95120@users.noreply.github.com>
2024-05-22 23:26:12 +0200

    Update Scene3Controller.java

--- JavaFX/Java/Scene3Controller.java
+++ JavaFX/Java/Scene3Controller.java
@@ -1,36 +1,19 @@
 package com.example.cybooks;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Scene3Controller {
-    private Stage stage;
-    private Scene scene;
+
     public void switchToScene6(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene6.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Main.switchScene("/com/example/cybooks/Scene6.fxml");
     }
+
     public void switchToScene7(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene7.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Main.switchScene("/com/example/cybooks/Scene7.fxml");
     }
+
     public void switchToScene8(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene8.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Main.switchScene("/com/example/cybooks/Scene8.fxml");
     }
 }
