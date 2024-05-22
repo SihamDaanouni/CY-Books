8c57d03036838f17de195f0c256b8e69af1765ed
Pascal95120 <140958680+Pascal95120@users.noreply.github.com>
2024-05-22 23:25:43 +0200

    Update Scene1Controller.java

--- JavaFX/Java/Scene1Controller.java
+++ JavaFX/Java/Scene1Controller.java
@@ -2,24 +2,12 @@ package com.example.cybooks;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Scene1Controller {
-    private Stage stage;
-    private Scene scene;
+
+    @FXML
     public void switchToConnexion(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/cybooks/Scene2.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Main.switchScene("/com/example/cybooks/Scene2.fxml");
     }
 }
