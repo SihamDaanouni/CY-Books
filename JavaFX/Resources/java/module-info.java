ff1bcc9b6ae4af0885bb36195f7e5e9d216a0e3c
Amaury-75 <82617886+Amaury-75@users.noreply.github.com>
2024-05-23 21:01:25 +0200

    nettoyage

--- JavaFX/Resources/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.scenesaminesiham {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.scenesaminesiham to javafx.fxml;
-    exports com.example.scenesaminesiham;
-}
